555c735db3679f9d7ca90b29c0c9aaa2a9190042
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-06-30 20:15:39 +0000

    Add summary page and run multipage class

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
+from app_pages.multipage import MultiPage
+
+from app_pages.page_summary import page_summary_body
 
 
+app = MultiPage(app_name='Purrfect Paws Predictor')
 
-st.write('Hello, world!')
+app.app_page('Project Summary', page_summary_body)
 
 
-st.info('Important information')
-
-st.success('I am green')
+app.run()
---
+++ app_pages/page_summary.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def page_summary_body():
+
+    st.write('Hello, world!')
+
+    st.info('Important information')
+
+    st.success('I am green')
