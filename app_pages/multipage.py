87b7295fcf1cbc9d2444f857a0525764136ec48d
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-06-30 19:55:33 +0000

    Fix typo in multipage class and add docstring

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -3,14 +3,29 @@ import streamlit as st
 
 
 class MultiPage:
+    """
+    A class to create a multi-page Streamlit app.
 
-    def __init__(selt, app_name) -> None:
+    Parameters:
+    app_name (str): The name of the app.
+
+    Attributes:
+    pages (list): A list of dictionaries, where each dictionary contains the title and function of a page.
+    app_name (str): The name of the app.
+
+    Methods:
+    app_page(title, func): Adds a new page to the app.
+        title (str): The title of the page.
+        func (function): The function to be executed when the page is selected.
+    run(): Runs the app.
+    """
+    def __init__(self, app_name) -> None:
         self.pages = []
         self.app_name = app_name
 
         st.set_page_config(
             page_title = self.app_name,
-            page_icon = ":desktop_computer:",
+            page_icon = ":cat2:",
         )
 
         def app_page(self, title, func) -> None:
