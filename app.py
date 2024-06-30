792d570631d428125160ca7ff39bce114cb7715e
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-06-30 20:23:58 +0000

    Add image study page

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 from app_pages.page_summary import page_summary_body
-
+from app_pages.page_image_study import page_image_study_body
 
 app = MultiPage(app_name='Purrfect Paws Predictor')
 
 app.app_page('Project Summary', page_summary_body)
-
+app.app_page('Image Study', page_image_study_body)
 
 app.run()
---
+++ app_pages/page_image_study.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def page_image_study_body():
+
+    st.write('Hello, world!')
+
+    st.info('Important information')
+
+    st.success('I am green')
