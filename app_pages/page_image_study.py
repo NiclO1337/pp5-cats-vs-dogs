dbeca5b684750743a246b401c8c6b95eb5e0393c
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-07-01 21:35:32 +0000

    Adjust image study page to pep8 standards

--- app_pages/page_image_study.py
+++ app_pages/page_image_study.py
@@ -5,21 +5,20 @@ from matplotlib.image import imread
 
 def page_image_study_body():
 
-    
     st.write('---')
 
     st.header('**Image study**')
 
     st.info('''
-Business requirement 1: Help clients understand how the model interprets 
+Business requirement 1: Help clients understand how the model interprets
 images and provide insights into distinctive features.
 ''')
 
     version = 'v1'
-    
+
     if st.checkbox(
         'Display the mean and standard deviation\
-        from the average image study'):
+            from the average image study'):
 
         avg_var_cat = plt.imread(f'outputs/{version}/avg_var_cat.png')
         avg_var_dog = plt.imread(f'outputs/{version}/avg_var_dog.png')
@@ -31,19 +30,17 @@ able to see any distinctive features in this image study.')
         st.image(avg_var_dog, caption='Dog image - Average and Variablility')
 
     if st.checkbox(
-        'Show the differences between the average cat and dog images.'):
+            'Show the differences between the average cat and dog images.'):
 
         avg_differences = plt.imread(f'outputs/{version}/avg_diff.png')
 
-        st.warning('Images in the dataset are too different so we are \
-not able to see any clear differences in this image study.')
+        st.warning('Images in the dataset are too different from eachother so \
+we are not able to see any clear differences in this image study.')
 
         st.image(avg_differences,
-        caption='Average differences between cat and dog images')
+                 caption='Average differences between cat and dog images')
 
     if st.checkbox(
-        'Display a montage of images from the dataset of both cats or dogs'):
+            'Display a montage of images from the \
+                dataset of both cats or dogs'):
         st.error('Hello')
-
-
-    
