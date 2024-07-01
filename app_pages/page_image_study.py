cab76d4f6eef7e763d32822d7397ef0caf9338bb
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-07-01 20:26:33 +0000

    Update BR and checkbox texts

--- app_pages/page_image_study.py
+++ app_pages/page_image_study.py
@@ -9,22 +9,24 @@ def page_image_study_body():
     st.header('**Image study**')
 
     st.error('''
-**Business requirement**
-
-
-The client requires us to do a study of their dataset and show them our 
-results.
+Business requirement 1: Help clients understand how the model interprets 
+images and provide insights into distinctive features.
 ''')
 
     version = 'v1'
     
-    if st.checkbox('Hello'):
+    if st.checkbox(
+        'Display the mean and standard deviation\
+        from the average image study'
+        ):
         st.warning('Hello')
 
-    if st.checkbox('Hello'):
+    if st.checkbox(
+        'Show the differences between the average cat and dog images.'):
         st.warning('Hello')
 
-    if st.checkbox('Hello'):
+    if st.checkbox(
+        'Display a montage of images from the dataset of both cats or dogs'):
         st.warning('Hello')
 
 
