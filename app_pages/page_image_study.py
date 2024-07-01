2324b80fd99a911f6b2ff4fa47209fdb9bfd0dda
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-07-01 20:55:38 +0000

    Add image and text to checkbox nr 2 of image study

--- app_pages/page_image_study.py
+++ app_pages/page_image_study.py
@@ -25,14 +25,21 @@ images and provide insights into distinctive features.
         avg_var_dog = plt.imread(f'outputs/{version}/avg_var_dog.png')
 
         st.success('There is too much variation in how images are taken to be \
-able to draw any valid conclusions from this average image study.')
+able to see any distinctive features in this image study.')
 
         st.image(avg_var_cat, caption='Cat image - Average and Variablility')
         st.image(avg_var_dog, caption='Dog image - Average and Variablility')
 
     if st.checkbox(
         'Show the differences between the average cat and dog images.'):
-        st.warning('Hello')
+
+        avg_differences = plt.imread(f'outputs/{version}/avg_diff.png')
+
+        st.warning('Images in the dataset are too different so we are \
+not able to see any clear differences in this image study.')
+
+        st.image(avg_differences,
+        caption='Average differences between cat and dog images')
 
     if st.checkbox(
         'Display a montage of images from the dataset of both cats or dogs'):
