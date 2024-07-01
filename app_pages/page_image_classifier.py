d2b47730811bcc489a05bd6b4ef294132d585793
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-07-01 21:57:59 +0000

    Add images buffer and import pandas

--- app_pages/page_image_classifier.py
+++ app_pages/page_image_classifier.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 
 def page_image_classifier_body():
-
     
     st.write('---')
 
@@ -17,3 +17,8 @@ enabling clients to test their HIP software.
 Click [**here**](link), 
 to download a set of cat and dog images for live prediction.''')
 
+    images_buffer = st.file_uploader('Upload images of cats or dogs here, \
+you can select more than one at the time.')
+
+    if images_buffer is not None:
+        df_report = pd.DataFrame([])
