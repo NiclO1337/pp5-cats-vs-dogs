7dad91e9927854b2546c2c7f7839d1091086f5b3
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-07-01 19:56:33 +0000

    Add text content to general info and dataset

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -9,15 +9,29 @@ def page_summary_body():
 
     st.info('''
 **General information**
-* information.  
-hello
 
-hello
+Web services are often protected with a challenge that's supposed to be easy 
+for people to solve, but difficult for computers. Such a challenge is often 
+called a CAPTCHA (Completely Automated Public Turing test to tell Computers 
+and Humans Apart) or HIP (Human Interactive Proof). HIPs are used for many 
+purposes, such as to reduce email and blog spam and prevent brute-force 
+attacks on web site passwords.
+
+Asirra (Animal Species Image Recognition for Restricting Access) is a HIP that 
+works by asking users to identify photographs of cats and dogs.
+
+As a Data Analyst from Code Institute Consulting, we are tasked with helping 
+Asirra improve on their HIP software. The current literature suggests that 
+machine classifiers can score above 80% accuracy on this task. Therefore, 
+Asirra is no longer considered safe from attack.
 ''')
 
     st.success('''
 **Project dataset**
 
+The dataset contains 25000 pictures of cats and dogs from Asirra in partnership
+with Petfinder.com. The images are taken and manually classified by people at 
+thousands of animal shelters. 
 ''')
 
     st.warning('''
