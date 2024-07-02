604d360e07d898ac0eb51e75c058c8e87cb43fae
Niclas Hugdahl <niclas.hugdahl@gmail.com>
2024-07-02 18:27:40 +0000

    Add label frequency and history to performance page

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
 
 
 def page_ml_performance_body():
@@ -7,25 +9,23 @@ def page_ml_performance_body():
 
     st.header('**ML Performance Metrics**')
 
-    st.info('''
-**General information**
-* information.  
-hello
+    version = 'v1'
 
-hello
-''')
+    st.info("Labels Frequencies in the dataset")
 
-    st.success('''
-**Project dataset**
+    labels_distribution = plt.imread(
+        f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution,
+             caption='Labels Distribution on Train, Validation and Test Sets')
 
-''')
+    st.write("---")
 
-    st.warning('''
-**Additional information**
-
-''')
-
-    st.error('''
-**Business requirements**
-
-''')
+    st.success("Model training history")
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+        st.image(model_acc, caption='Model Training Accuracy')
+    with col2:
+        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+        st.image(model_loss, caption='Model Training Losses')
+    st.write("---")
