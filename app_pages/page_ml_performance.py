036b7f1d1e893ae8e21bc8b61afbd263fc5ad096
NiclO1337 <niclas.hugdahl@gmail.com>
2026-08-26 09:15:44 +0200

    chore: upgrade python and dependencies to modern versions

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -23,7 +23,7 @@ def page_ml_performance_body():
     st.write("---")
 
     st.success("Model training history")
-    col1, col2 = st.beta_columns(2)
+    col1, col2 = st.columns(2)
     with col1:
         model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
         st.image(model_acc, caption='Model Training Accuracy')
